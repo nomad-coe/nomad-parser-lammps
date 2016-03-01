c74050d5e58344ee6c5bd940ac6b3564a52c4437
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-03-01 18:38:59 +0100

    Add parserId and versionInfo in parserInfo

--- src/main/scala/eu/nomad_lab/parsers/LammpsParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/LammpsParser.scala
@@ -1,12 +1,22 @@
 package eu.nomad_lab.parsers
+
+import eu.{nomad_lab=>lab}
 import eu.nomad_lab.DefaultPythonInterpreter
 import org.{json4s => jn}
+import scala.collection.breakOut
 
 object LammpsParser extends SimpleExternalParserGenerator(
   name = "LammpsParser",
   parserInfo = jn.JObject(
     ("name" -> jn.JString("LammpsParser")) ::
-      ("version" -> jn.JString("1.0")) :: Nil),
+      ("parserId" -> jn.JString("LammpsParser" + lab.LammpsVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+          (lab.LammpsVersionInfo.toMap.map{ case (key, value) =>
+            (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
   mainFileTypes = Seq("text/.*"),
   mainFileRe = """# input script for topotools""".r,
   cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/lammps/parser/parser-lammps/LAMMPSParserControl.py",
