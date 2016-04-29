4954e1820a44dd980ee3bdfc6df8aa3801430c28
Riello, Massimo (mriello) <massimo.riello@kcl.ac.uk>
2016-04-29 18:11:30 +0200

    fix

--- src/test/scala/eu/nomad_lab/parsers/LammpsParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/LammpsParserSpec.scala
@@ -8,10 +8,10 @@ import org.{ json4s => jn }
 object LammpsParserSpec extends Specification {
   “LammpsParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(VaspParser, "test/examples/methane/input.methane_nvt_thermo_style_custom", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(LammpsParser, "test/examples/methane/input.methane_nvt_thermo_style_custom", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(VaspParser, "test/examples/methane/input.methane_nvt_thermo_style_custom", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(LammpsParser, "test/examples/methane/input.methane_nvt_thermo_style_custom", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
