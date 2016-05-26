436c7ed87201ba3e21bec16a45094bd15b64edfe
Massimo Riello <maxoriello@Massimos-MacBook-Air.local>
2016-05-26 11:09:39 +0100

    testing on different logging styles

--- src/test/scala/eu/nomad_lab/parsers/LammpsParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/LammpsParserSpec.scala
@@ -12,3 +12,36 @@ object LammpsParserSpec extends Specification {
     }
   }
 }
+
+object LammpsParserSpec extends Specification {
+  "LammpsParserTest" >> {
+    "test with json-events" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/methane/input.methane_nvt_thermo_style_one", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/methane/input.methane_nvt_thermo_style_one", "json") must_== ParseResult.ParseSuccess
+    }
+  }
+}
+
+object LammpsParserSpec extends Specification {
+  "LammpsParserTest" >> {
+    "test with json-events" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/methane/input.methane_nvt_thermo_style_multi", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/methane/input.methane_nvt_thermo_style_multi", "json") must_== ParseResult.ParseSuccess
+    }
+  }
+}
+
+object LammpsParserSpec extends Specification {
+  "LammpsParserTest" >> {
+    "test with json-events" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/1_methyl_naphthalene/input.1_methyl_naphthalene", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(LammpsParser, "parsers/lammps/test/examples/1_methyl_naphthalene/input.1_methyl_naphthalene", "json") must_== ParseResult.ParseSuccess
+    }
+  }
+}
