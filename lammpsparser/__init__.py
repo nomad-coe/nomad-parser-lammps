7ed44cab4410099208f5e399bfa4c1dc2e9c29fe
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-09-21 11:23:59 +0200

    Changed the code_name to the one used in our index.

--- lammpsparser/__init__.py
+++ lammpsparser/__init__.py
@@ -7,7 +7,7 @@ from lammpsparser.lammps_parser import LammpsOutput
 class LammpsParser(FairdiParser):
     def __init__(self):
         super().__init__(
-            name='parsers/lammps', code_name='lammps', code_homepage='https://lammps.sandia.gov/',
+            name='parsers/lammps', code_name='LAMMPS', code_homepage='https://lammps.sandia.gov/',
             domain='dft', mainfile_contents_re=r'^LAMMPS')
 
     def parse(self, filepath, archive, logger=None):
